2cfbe749b40dc33a38eb03b69a18cffa47b2518a
lxliangit <freeheart.steven@gmail.com>
2016-09-27 17:16:33 +0800

    add /github/ to LoFreq_indelovlp_filtervcf_vcf2avinput_annovar/vcf2avinput_annovar_lofreq2_1_1.sh and LoFreq_indelovlp_filtervcf_vcf2avinput_annovar/vcf2avinput_annovar_lofreq2_1_1.sh

--- LoFreq_indelovlp_filtervcf_vcf2avinput_annovar/vcf2avinput_annovar_lofreq2_1_2.sh
+++ LoFreq_indelovlp_filtervcf_vcf2avinput_annovar/vcf2avinput_annovar_lofreq2_1_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 annovar_dir="/mnt/projects/liuxl/ctso4_projects/liuxl/Tools/annovar"
-annovar_header=/mnt/projects/liuxl/ctso4_projects/liuxl/scripts/LoFreq_indelovlp_filtervcf_vcf2avinput_annovar/Annovar_header_lofreq2_1_2.txt
+annovar_header=/mnt/projects/liuxl/ctso4_projects/liuxl/scripts/github/LoFreq_indelovlp_filtervcf_vcf2avinput_annovar/Annovar_header_lofreq2_1_2.txt
 
 vcf=$1
 vartype=$2
@@ -16,11 +16,11 @@ fi
 
 if [[ $vartype != "" ]] 
 then
-    java -Xmx$Xmx -XX:+UseSerialGC -cp /mnt/projects/liuxl/ctso4_projects/liuxl/scripts/LoFreq_indelovlp_filtervcf_vcf2avinput_annovar:. LoFreqVCF2Avinput_lofreq2_1_2 $vcf > $vcf.avinput
+    java -Xmx$Xmx -XX:+UseSerialGC -cp /mnt/projects/liuxl/ctso4_projects/liuxl/scripts/github/LoFreq_indelovlp_filtervcf_vcf2avinput_annovar:. LoFreqVCF2Avinput_lofreq2_1_2 $vcf > $vcf.avinput
     perl $annovar_dir/table_annovar.pl -buildver hg19 -remove -outfile ${vcf}_${vartype} -protocol refGene,ensGene,cytoBand,snp138,snp138NonFlagged,cosmic70,ljb26_all,avsift,tfbsConsSites,wgRna,targetScanS,gwasCatalog,esp6500si_all,1000g2012apr_all -operation g,g,r,f,f,f,f,f,r,r,r,r,f,f -otherinfo $vcf.avinput $annovar_dir/humandb/
         
     tail -n +2 ${vcf}_${vartype}.hg19_multianno.txt | cat $annovar_header - > ${vcf}_${vartype}_hg19_multianno.txt
-    perl /mnt/projects/liuxl/ctso4_projects/liuxl/scripts/VOGELSTEIN/add_annotations_of_driver_mut.pl ${vcf}_${vartype}_hg19_multianno.txt
+    perl /mnt/projects/liuxl/ctso4_projects/liuxl/scripts/github/VOGELSTEIN/add_annotations_of_driver_mut.pl ${vcf}_${vartype}_hg19_multianno.txt
 else
     printf "please specify which types of variants: snv, indel, allvar\n"
 fi
