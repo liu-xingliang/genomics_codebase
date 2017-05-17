6b3b88a5bc614444c5f3363fe00ef9d366bdf151
lxliangit <freeheart.steven@gmail.com>
2017-05-17 16:49:09 +0800

    add Xmx and mem_free to ovlp_and_rmprimer_splitchr/submit_ovlp_and_rmprimer_splitchr_aquila.sh

--- ovlp_and_rmprimer_splitchr/submit_ovlp_and_rmprimer_splitchr_aquila.sh
+++ ovlp_and_rmprimer_splitchr/submit_ovlp_and_rmprimer_splitchr_aquila.sh
@@ -4,6 +4,8 @@
 
 not_empty_chrlist=$2 #/mnt/projects/liuxl/ctso4_projects/liuxl/SayLi/JCO/lungprimer/not_empty/locusbased_rmprimer/human_g1k_v37_chrlist_not_empty
 primer_dir=$3 # /mnt/projects/liuxl/ctso4_projects/liuxl/SayLi/dataquality_2016Apr/BC50_primers/not_empty/locusbased_rmprimer/human_g1k_v37/ 
+mem_free=$4
+Xmx=$5
 
 perl_scripts=/mnt/projects/liuxl/ctso4_projects/liuxl/scripts/github/ovlp_and_rmprimer_splitchr/ovlp_and_rmprimer_splitchr_locusebased_aquila.pl # need to be changed for different version of perl_scripts 
 cp  $perl_scripts .
