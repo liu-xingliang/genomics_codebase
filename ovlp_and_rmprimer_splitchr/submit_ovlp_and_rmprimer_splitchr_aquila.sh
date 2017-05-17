ccc8fa35218354b274652cec925e06a175cdfecf
lxliangit <freeheart.steven@gmail.com>
2017-05-17 17:00:35 +0800

    add Xmx and mem_free to ovlp_and_rmprimer_splitchr/submit_ovlp_and_rmprimer_splitchr_aquila.sh (cont.)

--- ovlp_and_rmprimer_splitchr/submit_ovlp_and_rmprimer_splitchr_aquila.sh
+++ ovlp_and_rmprimer_splitchr/submit_ovlp_and_rmprimer_splitchr_aquila.sh
@@ -20,7 +20,7 @@ do
 
     cd ${bam}_d
 
-    perl $perl_scripts $bam 30G 20G \
+    perl $perl_scripts $bam $Xmx $mem_free \
     $not_empty_chrlist \
     "/mnt/projects/liuxl/ctso4_projects/liuxl/myTools/OverlapBaseRefine.jar" \
     /mnt/projects/liuxl/ctso4_projects/liuxl/myTools/PrimerTrimmer_LocusBased_IndelQual_BAQ.jar \
